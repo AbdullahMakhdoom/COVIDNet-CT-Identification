460d00e0a8a0ccf307c403cc0e48f459b4ea7d63
Abdullah Makhdoom <30556653+AbdullahMakhdoom@users.noreply.github.com>
2022-04-24 02:40:23 +0500

    Update augmentations.py

--- augmentations.py
+++ augmentations.py
@@ -1,9 +1,6 @@
 import math
 import tensorflow as tf
 
-from data_utils import exterior_exclusion
-
-
 def random_rotation(image, max_degrees, bbox=None, prob=0.5):
     """Applies random rotation to image and bbox"""
     def _rotation(image, bbox):
@@ -96,6 +93,26 @@ def random_shear(image, max_lambda, bbox=None, prob=0.5):
     retval = image if bbox is None else (image, bbox)
     return tf.cond(_should_apply(prob), lambda: _shear(image, bbox), lambda: retval)
 
+def exterior_exclusion(image):
+    """Removes visual features exterior to the patient's body"""
+    # Create initial binary image
+    filt_image = cv2.GaussianBlur(image, (5, 5), 0)
+    filt_image.shape = image.shape  # ensure channel dimension is preserved if present
+    thresh = cv2.threshold(filt_image[filt_image > 0], 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[0]
+    bin_image = filt_image > thresh
+
+    # Find body contour
+    body_cont = body_contour(bin_image.astype(np.uint8))
+
+    # Exclude external regions by replacing with bg mean
+    body_mask = np.zeros(image.shape, dtype=np.uint8)
+    cv2.drawContours(body_mask, [body_cont], 0, 1, -1)
+    body_mask = body_mask.astype(bool)
+    bg_mask = (~body_mask) & (image > 0)
+    bg_dark = bg_mask & (~bin_image)  # exclude bright regions from mean
+    bg_mean = np.mean(image[bg_dark])
+    image[bg_mask] = bg_mean
+    return image
 
 def random_exterior_exclusion(image, prob=0.5):
     """Randomly removes visual features exterior to the patient's body"""
